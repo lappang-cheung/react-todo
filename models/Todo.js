94293445a7b443815866875bccc3574fe2d9059c
lemonjai <lappangcheung@gmail.com>
2018-06-12 23:36:39 -0400

    able to save post from postman

--- models/Todo.js
+++ models/Todo.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const TodoSchema = new Schema({
-    text: {
+    description: {
         type: String,
         required: true
     }
--- routes/api/todo.js
+++ routes/api/todo.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Todo = require('../../models/Todo');
 
 // @route   GET api/todos/test
 // @desc    Test todos route
 // @access  Public
 router.get('/test', (req,res) => res.json({msg: "To do works"}));
+
+// @route   POST api/todos
+// @desc    Create post
+// @access  Public
+router.post('/new', (req,res) => {
+    const todo = new Todo({
+        description: req.body.text
+    });
+
+    todo.save()
+        .then(todo => res.json(todo))
+})
 
 module.exports = router;
