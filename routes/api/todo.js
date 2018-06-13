d640a315b939e4bb77a4e90b6f154368d0b45859
lemonjai <lappangcheung@gmail.com>
2018-06-12 23:58:08 -0400

    able to delete task from id on postman

--- routes/api/todo.js
+++ routes/api/todo.js
@@ -20,6 +20,19 @@ router.post('/new', (req,res) => {
         .then(todo => res.json(todo))
 });
 
+// @route   POST api/todos
+// @desc    Delete post
+// @access  Public
+router.delete('/:id', (req,res) => {
+    Todo.findById(req.params.id)
+        .then(todo => {
+            todo.remove().then(() => res.json({
+                success: true
+            }))
+            .catch(err => res.status(404).json({todonotfound: 'Task item not found'}))
+        })
+})
+
 // @route   Get api/todos
 // @desc    Get posts
 // @access  Pulic
@@ -27,7 +40,9 @@ router.get('/', (req,res) => {
     Todo.find()
         .sort({date: -1})
         .then(todos => res.json(todos))
-        .catch(err => res.status(404));
+        .catch(err => res.status(404).json({
+            notodofound: 'No task items found'
+        }));
 });
 
 module.exports = router;
